2332bb3dd1b4f6279e33c66b3873aca219cb6519
Sebastien <sebastien@sebs-studio.com>
2015-06-24 23:55:51 +0200

    First commit

---
+++ wc-vendor-registration/templates/vendor-registration-form.php
@@ -0,0 +1,68 @@
+<form method="post" class="register">
+
+	<?php do_action( 'ss_wc_vendor_registration_form_start' ); ?>
+
+	<?php if ( $message ) echo wpautop( wptexturize( $message ) ); ?>
+
+	<p class="form-row form-row-first">
+		<label for="reg_vendor_first_name"><?php _e( 'First name', 'ss-wc-vendor-registration' ); ?> <span class="required">*</span></label>
+		<input type="text" class="input-text" name="vendor_first_name" id="reg_vendor_first_name" value="<?php if ( ! empty( $_POST['vendor_first_name'] ) ) esc_attr_e( $_POST['vendor_first_name'] ); ?>" />
+	</p>
+
+	<p class="form-row form-row-last">
+		<label for="reg_vendor_last_name"><?php _e( 'Last name', 'ss-wc-vendor-registration' ); ?> <span class="required">*</span></label>
+		<input type="text" class="input-text" name="vendor_last_name" id="reg_vendor_last_name" value="<?php if ( ! empty( $_POST['vendor_last_name'] ) ) esc_attr_e( $_POST['vendor_last_name'] ); ?>" />
+	</p>
+
+	<p class="form-row form-row-first">
+		<label for="username"><?php _e( 'Username or email', 'ss-wc-vendor-registration' ); ?> <span class="required">*</span></label>
+		<input type="text" class="input-text" name="username" id="username" />
+	</p>
+	<p class="form-row form-row-last">
+		<label for="password"><?php _e( 'Password', 'ss-wc-vendor-registration' ); ?> <span class="required">*</span></label>
+		<input class="input-text" type="password" name="password" id="password" />
+	</p>
+
+	<div class="clear"></div>
+
+	<p class="form-row form-row-wide">
+		<h3><?php _e( 'Your Store Details', 'ss-wc-vendor-registration' ); ?></h3>
+	</p>
+
+	<p class="form-row form-row-first">
+		<label for="store_name"><?php _e( 'Store Name', 'ss-wc-vendor-registration' ); ?> <span class="required">*</span></label>
+		<input type="text" class="input-text" name="store_name" id="store_name" value="<?php if ( ! empty( $_POST['store_name'] ) ) esc_attr_e( $_POST['store_name'] ); ?>" />
+		<span><?php _e( 'What is the name of your store?', 'ss-wc-vendor-registration' ); ?></span>
+	</p>
+	<p class="form-row form-row-last">
+		<label for="paypal_email"><?php _e( 'PayPal Email Address', 'ss-wc-vendor-registration' ); ?> <span class="required">*</span></label>
+		<input type="text" class="input-text" name="paypal_email" id="paypal_email" value="<?php if ( ! empty( $_POST['paypal_email'] ) ) esc_attr_e( $_POST['paypal_email'] ); ?>" />
+		<span><?php _e( 'The PayPal email address of the vendor where their profits will be delivered.', 'ss-wc-vendor-registration' ); ?></span>
+	</p>
+
+	<p class="form-row form-row-wide">
+		<label for="reg_store_description"><?php _e( 'Store Description', 'ss-wc-vendor-registration' ); ?></label>
+		<textarea name="store_description" id="reg_store_description" cols="30" rows="10"><?php echo isset( $_POST['store_description'] ) ? $_POST['store_description'] : '' ?></textarea>
+	</p>
+
+	<?php do_action( 'ss_wc_vendor_registration_form' ); ?>
+
+	<p class="form-row">
+		<?php wp_nonce_field( 'ss-wc-vendor-registration-form' ); ?>
+		<input type="submit" class="button" name="register" value="<?php _e( 'Register', 'ss-wc-vendor-registration' ); ?>" />
+		<input type="hidden" name="redirect" value="<?php echo esc_url( $redirect ) ?>" />
+	</p>
+
+	<p class="alread_registered">
+		<?php _e( 'Already Registered?', 'ss-wc-vendor-registration' ); ?> <a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>"><?php _e( 'Login', 'ss-wc-vendor-registration' ); ?></a>
+	</p>
+
+	<p class="form-row form-row-wide">
+		
+	</p>
+
+	<div class="clear"></div>
+
+	<?php do_action( 'ss_wc_vendor_registration_form_end' ); ?>
+
+</form>
---
+++ wc-vendor-registration/wc-vendor-registration.php
@@ -0,0 +1,361 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Vendor Registration
+ * Plugin URI:  https://github.com/Sebs-Studio/WooCommerce-Vendor-Registration
+ * Description: Allows users to register as a vendor and create a store on your e-commerce marketplace site and add their products. Requires WooCommerce Product Vendors!
+ * Version:     0.0.1
+ * Author:      Sebs Studio
+ * Author URI:  http://www.sebs-studio.com
+ * Text Domain: ss-wc-vendor-registration
+ * Domain Path: languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+## What the plugin does!
+## ---------------------
+## Register User as new user role called Vendor.
+## Validate form details
+## Create vendor shop
+## Provide a shortcode to insert registration page.
+## ------------------------------------------------
+
+/**
+ * Create user role and user capabilities for vendor.
+ *
+ * @since  0.0.1
+ * @access public
+ * @global $wp_roles
+ * @see    http://codex.wordpress.org/Roles_and_Capabilities
+ */
+function ss_wc_create_user_role_vendor() {
+	global $wp_roles;
+
+	if ( class_exists( 'WP_Roles' ) ) {
+		if ( ! isset( $wp_roles ) ) {
+			$wp_roles = new WP_Roles();
+		}
+	}
+
+	if ( is_object( $wp_roles ) ) {
+
+		add_role( 'vendor', __( 'Vendor', 'ss-wc-vendor-registration' ), array(
+			'level_9'                => false,
+			'level_8'                => false,
+			'level_7'                => false,
+			'level_6'                => false,
+			'level_5'                => false,
+			'level_4'                => false,
+			'level_3'                => true,
+			'level_2'                => true,
+			'level_1'                => true,
+			'level_0'                => true,
+			'read'                   => true,
+			'read_private_pages'     => false,
+			'read_private_posts'     => false,
+			'edit_users'             => false,
+			'edit_posts'             => false,
+			'edit_pages'             => false,
+			'edit_published_posts'   => false,
+			'edit_published_pages'   => false,
+			'edit_private_pages'     => false,
+			'edit_private_posts'     => false,
+			'edit_others_posts'      => true,
+			'edit_others_pages'      => true,
+			'publish_posts'          => false,
+			'publish_pages'          => false,
+			'delete_posts'           => false,
+			'delete_pages'           => false,
+			'delete_private_pages'   => false,
+			'delete_private_posts'   => false,
+			'delete_published_pages' => false,
+			'delete_published_posts' => false,
+			'delete_others_posts'    => true,
+			'delete_others_pages'    => true,
+			'manage_categories'      => false,
+			'manage_links'           => false,
+			'moderate_comments'      => false,
+			'unfiltered_html'        => false,
+			'upload_files'           => true,
+			'export'                 => false,
+			'import'                 => false,
+			'list_users'             => false
+		) );
+
+		$capabilities = ss_wc_vendor_register_get_core_capabilities();
+
+		foreach( $capabilities as $cap_group ) {
+			foreach( $cap_group as $cap ) {
+				$wp_roles->add_cap( 'vendor', $cap );
+			}
+		}
+	}
+} // END ss_wc_create_user_role_vendor()
+
+/**
+ * Get capabilities for WooCommerce Vendor Registration.
+ *
+ * These are assigned to admin and any other
+ * user role capabilities during installation
+ * or resetting the plugin.
+ *
+ * @access public
+ * @filter ss_wc_vendor_register_capability_post_types
+ * @return array
+ */
+function ss_wc_vendor_register_get_core_capabilities() {
+	$capabilities = array();
+
+	$capabilities['core'] = array(
+		'manage_woocommerce',
+		'view_woocommerce_reports'
+	);
+
+	$capability_types = apply_filters( 'ss_wc_vendor_register_capability_post_types', array( 'media', 'product', 'shop_order', 'shop_coupon' ) );
+
+	foreach( $capability_types as $capability_type ) {
+
+		$capabilities[ $capability_type ] = array(
+			// Post type
+			"edit_{$capability_type}",
+			"read_{$capability_type}",
+			"delete_{$capability_type}",
+			"edit_{$capability_type}s",
+			"edit_others_{$capability_type}s",
+			//"publish_{$capability_type}s",
+			"read_private_{$capability_type}s",
+			"delete_{$capability_type}s",
+			"delete_private_{$capability_type}s",
+			"delete_published_{$capability_type}s",
+			"delete_others_{$capability_type}s",
+			"edit_private_{$capability_type}s",
+			"edit_published_{$capability_type}s",
+
+			// Terms
+			"manage_{$capability_type}_terms",
+			"edit_{$capability_type}_terms",
+			"delete_{$capability_type}_terms",
+			"assign_{$capability_type}_terms",
+			"upload_files",
+			"manage_bookings",
+		);
+	}
+
+	return $capabilities;
+} // END ss_wc_vendor_register_get_core_capabilities()
+
+function ss_wc_vendor_remove_roles() {
+	global $wp_roles;
+
+	if ( ! class_exists( 'WP_Roles' ) ) {
+		return;
+	}
+
+	if ( ! isset( $wp_roles ) ) {
+		$wp_roles = new WP_Roles();
+	}
+
+	$capabilities = ss_wc_vendor_register_get_core_capabilities();
+
+	foreach ( $capabilities as $cap_group ) {
+		foreach ( $cap_group as $cap ) {
+		$wp_roles->remove_cap( 'vendor', $cap );
+		}
+	}
+
+	remove_role( 'vendor' );
+} // END ss_wc_vendor_remove_roles()
+
+/**
+ * Create a new vendor
+ *
+ * @param  string $email
+ * @param  string $username
+ * @param  string $password
+ * @return int|WP_Error on failure, Int (user ID) on success
+ */
+function ss_wc_create_new_vendor( $email, $username = '', $password = '' ) {
+
+	// Check the e-mail address
+	if ( empty( $email ) || ! is_email( $email ) ) {
+		return new WP_Error( 'registration-error', __( 'Please provide a valid email address.', 'ss-wc-vendor-registration' ) );
+	}
+
+	if ( email_exists( $email ) ) {
+		return new WP_Error( 'registration-error', __( 'An account is already registered with your email address. Please login.', 'ss-wc-vendor-registration' ) );
+	}
+
+	// Handle username creation
+	if ( 'no' === get_option( 'woocommerce_registration_generate_username' ) || ! empty( $username ) ) {
+
+		$username = sanitize_user( $username );
+
+		if ( empty( $username ) || ! validate_username( $username ) ) {
+			return new WP_Error( 'registration-error', __( 'Please enter a valid account username.', 'ss-wc-vendor-registration' ) );
+		}
+
+		if ( username_exists( $username ) )
+			return new WP_Error( 'registration-error', __( 'An account is already registered with that username. Please choose another.', 'ss-wc-vendor-registration' ) );
+	} else {
+
+		$username = sanitize_user( current( explode( '@', $email ) ), true );
+
+		// Ensure username is unique
+		$append     = 1;
+		$o_username = $username;
+
+		while ( username_exists( $username ) ) {
+			$username = $o_username . $append;
+			$append ++;
+		}
+	}
+
+	// Handle password creation
+	if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && empty( $password ) ) {
+		$password = wp_generate_password();
+		$password_generated = true;
+
+	} elseif ( empty( $password ) ) {
+		return new WP_Error( 'registration-error', __( 'Please enter an account password.', 'ss-wc-vendor-registration' ) );
+
+	} else {
+		$password_generated = false;
+	}
+
+	// WP Validation
+	$validation_errors = new WP_Error();
+
+	do_action( 'ss_wc_register_vendor', $username, $email, $validation_errors );
+
+	$validation_errors = apply_filters( 'woocommerce_registration_errors', $validation_errors, $username, $email );
+
+	if ( $validation_errors->get_error_code() )
+		return $validation_errors;
+
+	$new_vendor_data = apply_filters( 'ss_wc_new_vendor_data', array(
+		'user_login' => $username,
+		'user_pass'  => $password,
+		'user_email' => $email,
+		'role'       => 'vendor'
+	) );
+
+	$vendor_id = wp_insert_user( $new_vendor_data );
+
+	if ( is_wp_error( $vendor_id ) ) {
+		return new WP_Error( 'registration-error', '<strong>' . __( 'ERROR', 'ss-wc-vendor-registration' ) . '</strong>: ' . __( 'Couldn&#8217;t register you&hellip; please contact us if you continue to have problems.', 'ss-wc-vendor-registration' ) );
+	}
+
+	do_action( 'ss_wc_created_vendor', $vendor_id, $new_vendor_data, $password_generated );
+
+	return $vendor_id;
+}
+
+function ss_wc_validate_register_fields() {
+	if ( isset( $_POST['vendor_first_name'] ) && empty( $_POST['vendor_first_name'] ) ) {
+		$validation_errors->add( 'vendor_first_name_error', __( '<strong>Error</strong>: First name is required!', 'ss-wc-vendor-registration' ) );
+	}
+
+	if ( isset( $_POST['vendor_last_name'] ) && empty( $_POST['vendor_last_name'] ) ) {
+		$validation_errors->add( 'vendor_last_name_error', __( '<strong>Error</strong>: Last name is required!.', 'ss-wc-vendor-registration' ) );
+	}
+} // END ss_wc_validate_register_fields()
+add_action( 'ss_wc_register_vendor', 'ss_wc_validate_register_fields', 10, 3 );
+
+// Saves the vendors details.
+function ss_wc_save_vendor_details( $vendor_id ) {
+	if ( isset( $_POST['vendor_first_name'] ) ) {
+		// WordPress default first name field.
+		update_user_meta( $vendor_id, 'first_name', sanitize_text_field( $_POST['vendor_first_name'] ) );
+
+		// WooCommerce Vendor first name.
+		update_user_meta( $vendor_id, 'vendor_first_name', sanitize_text_field( $_POST['vendor_first_name'] ) );
+	}
+
+	if ( isset( $_POST['vendor_last_name'] ) ) {
+		// WordPress default last name field.
+		update_user_meta( $vendor_id, 'last_name', sanitize_text_field( $_POST['vendor_last_name'] ) );
+
+		// WooCommerce Vendor last name.
+		update_user_meta( $vendor_id, 'vendor_last_name', sanitize_text_field( $_POST['vendor_last_name'] ) );
+	}
+}
+add_action( 'ss_wc_created_vendor', 'ss_wc_save_vendor_details' );
+
+// Create the vendors store.
+function ss_wc_create_vendor_store( $vendor_id ) {
+	$store_details = array(
+		'name'              => trim( $_POST['store_name'] ),
+		'description'       => trim( $_POST['store_description'] ),
+		'vendor_admins'     => $vendor_id,
+		'vendor_commission' => '', // Left blank, up to the admin to decide commission rate.
+		'paypal_email'      => $_POST['paypal_email']
+	);
+
+	// Create vendor store.
+	$new_vendor_id = wp_insert_term( $store_details['name'], 'shop_vendor', array(
+		'description' => $shop_details['description'],
+		'slug'        => trim( strtolower( str_replace( ' ', '-', $shop_details['name'] ) ) ),
+		'parent'      => ''
+	) );
+
+	if( ! is_wp_error( $new_vendor_id ) ) {
+		// Get term id, set default to 0 if not set.
+		$new_vendor_id = isset( $new_vendor_id['term_id'] ) ? $new_vendor_id['term_id'] : 0;
+
+		do_action( 'ss_wc_save_vendor_details', $vendor_id, $new_vendor_id, $store_details );
+	}
+
+} // END ss_wc_create_vendor_store()
+add_action( 'ss_wc_created_vendor', 'ss_wc_create_vendor_store' );
+
+// The Token
+function ss_wc_vendor_reg_get_token() {
+	return 'shop_vendor';
+} // END ss_wc_vendor_reg_get_token()
+
+// Save the store details once the store has been created.
+function ss_wc_save_store_details( $vendor_id, $new_vendor_id, $store_details ) {
+	if( ! $vendor_id ) {
+		return;
+	}
+
+	// PayPal account email address
+	$paypal_address = $store_details['paypal_email'];
+	$vendor_data['paypal_email'] = $paypal_address;
+
+	// Commission
+	$commission = $store_details['commission'];
+	$vendor_date['vendor_commission'] = $commission;
+
+	// Apply the ID of the Vendor
+	$vendor = $store_details['vedor_admins'];
+	$vendor_date['vendor_admins'] = $vendor;
+
+	update_option( ss_wc_vendor_reg_get_token() . '_' . $new_vendor_id, $vendor_data );
+
+	// Vendor description
+	$args = array(
+		'description' => $store_details['description']
+	);
+
+	wp_update_term( $new_vendor_id, ss_wc_vendor_reg_get_token(), $args );
+} // END ss_wc_save_store_details()
+add_action( 'ss_wc_save_vendor_details', 'ss_wc_save_store_details', 3 );
+
+/**
+ * Insert the vendor regsitration form where you 
+ * want using this shortcode.
+ *
+ * @how Insert [vendor_registration] in your page.
+ * @version 0.0.1
+ */
+function ss_wc_register_vendor_form_shortcode() {
+	// If the user is already logged in, return nothing.
+	if ( is_user_logged_in() ) { return; }
+
+	if ( ! is_user_logged_in() ) {
+		include_once( 'templates/vendor-registration-form.php' );
+	}
+
+} // END ss_wc_register_vendor_form_shortcode()
+add_shortcode( 'vendor_registration', 'ss_wc_register_vendor_form_shortcode' );
